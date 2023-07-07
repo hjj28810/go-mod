b13c3b2212b9e6a1cc48c74bbc84db743f0af634
Alvin Huang <huangjunjie@lanyife.com>
2023-07-07 15:23:34 +0800

    修改初始化参数

--- utility/config.go
+++ utility/config.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"sync"
 )
 
 type configType int
@@ -29,8 +28,6 @@ func (c configType) ToPath(path string) string {
 	}
 }
 
-var instanceOnce sync.Once
-
 // 从配置文件中载入json字符串
 func LoadConfig[T any](path string) T {
 	buf, err := os.ReadFile(path)
@@ -45,14 +42,15 @@ func SetConfig[T any](path string) T {
 	return LoadConfig[T](path)
 }
 
-// 初始化，只运行一次
+// var instanceOnce sync.Once
+
+// 初始化
 func InitConfig[T any](path string) T {
-	filePath := GetCurrentPath()
-	var result T
-	instanceOnce.Do(func() {
-		result = LoadConfig[T](filePath + path)
-	})
-	return result
+	// var result T
+	// instanceOnce.Do(func() {  //只运行一次
+	// 	result = LoadConfig[T](filePath + path)
+	// })
+	return LoadConfig[T](GetCurrentPath() + path)
 }
 
 func CheckErr(err error) {

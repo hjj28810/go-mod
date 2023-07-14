1d25870146b3d79381f3eaa5161b840b96702b19
Alvin Huang <huangjunjie@lanyife.com>
2023-07-14 09:48:11 +0800

    提交合并

--- utility/config.go
+++ utility/config.go
@@ -30,9 +30,9 @@ func (c configType) ToPath(path string) string {
 }
 
 // 从配置文件中载入json字符串
-func LoadConfig[T any](path string, notDefault ...bool) T {
+func LoadConfig[T any](path string, isDefault ...bool) T {
 	var filePath string
-	if len(notDefault) == 0 || !notDefault[0] {
+	if len(isDefault) == 0 || isDefault[0] {
 		filePath = GetCurrentPath()
 	}
 	buf, err := os.ReadFile(filePath + path)
@@ -43,17 +43,17 @@ func LoadConfig[T any](path string, notDefault ...bool) T {
 }
 
 // 初始化 可以运行多次
-func SetConfig[T any](path string, notDefault ...bool) T {
-	return LoadConfig[T](path, notDefault...)
+func SetConfig[T any](path string, isDefault ...bool) T {
+	return LoadConfig[T](path, isDefault...)
 }
 
 var instanceOnce sync.Once
 
 // 初始化一次
-func InitConfig[T any](path string, notDefault ...bool) T {
+func InitConfig[T any](path string, isDefault ...bool) T {
 	var result T
 	instanceOnce.Do(func() {
-		result = LoadConfig[T](path, notDefault...)
+		result = LoadConfig[T](path, isDefault...)
 	})
 	return result
 }
